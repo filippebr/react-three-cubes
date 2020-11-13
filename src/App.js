503208548902a17326339d87b98525ccc86156dd
Filippe Braga <filippeffx@hotmail.com>
2020-11-13 20:55:40 -0300

    Added OrbitControls

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.scss';
 
 import { Canvas, useFrame } from 'react-three-fiber';
 
-import { softShadows, MeshWobbleMaterial } from 'drei';
+import { softShadows, MeshWobbleMaterial, OrbitControls } from 'drei';
 
 softShadows();
 
-const SpinningMesh = ({ position, args, color }) => {
+const SpinningMesh = ({ position, args, color, speed }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += .01));
   return (
@@ -16,8 +16,8 @@ const SpinningMesh = ({ position, args, color }) => {
       <MeshWobbleMaterial 
         attach='material' 
         color={color} 
-        speed={3} 
-        factor={.6} 
+        speed={speed} 
+        factor={.5} 
       />
     </mesh>
   )
@@ -58,9 +58,15 @@ function App() {
           </mesh>
         </group>
 
-        <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color="lightblue" />
-        <SpinningMesh position={[-2, 1, -5]} color="pink" />
-        <SpinningMesh position={[5, 1, -2]} color="pink" />
+        <SpinningMesh 
+          position={[0, 1, 0]} 
+          args={[3, 2, 1]} 
+          color="lightblue" 
+          speed={2} 
+        />
+        <SpinningMesh position={[-2, 1, -5]} color="pink" speed={6} />
+        <SpinningMesh position={[5, 1, -2]} color="pink" speed={6} />
+        <OrbitControls />
       </Canvas>
     </>
   );
